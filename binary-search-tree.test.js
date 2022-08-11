fbfe5bf73f021e1de1e96c53243ad7f7efd51d1a
lnicepei <dmitrii.shaidorov@gmail.com>
2022-08-11 12:06:29 +0300

    Implement many tests

--- binary-search-tree.test.js
+++ binary-search-tree.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { Tree } from "./binary-search-tree";
 
 const newTree = new Tree([
@@ -134,24 +135,77 @@ describe("Methods of the tree", () => {
     });
   });
 
-  it(`levelOrder function should return an array of
-   values if no function is provided. Otherwise it should `, () => {
-    expect(newTree.levelOrder()).toEqual(
-      expect.arrayContaining([
-        2, 6, 235, 5, 236, 23, 62, 52, 345, 21, 35, 45, 346,
-      ])
-    );
+  it("levelOrder function should return an array of BST values", () => {
+    expect(newTree.levelOrder(newTree.root)).toEqual([
+      45, 6, 235, 2, 23, 52, 345, 5, 21, 35, 62, 236, 346,
+    ]);
   });
 
-  it("height method should return how deep the given node is", () => {
-    expect(newTree.height(newTree.root.leftPart.rightPart)).toBe(1);
+  describe("Height method", () => {
+    it("this method should return the height of some node in the tree", () => {
+      expect(newTree.height(newTree.root.leftPart.rightPart)).toBe(1);
+    });
+
+    it("height method should return how deep the given node is", () => {
+      expect(newTree.height(newTree.root.rightPart.leftPart.rightPart)).toBe(0);
+    });
+
+    it("this method should return the height of the tree", () => {
+      expect(newTree.height(newTree.root)).toBe(3);
+    });
   });
 
-  it("height method should return how deep the given node is", () => {
-    expect(newTree.height(newTree.root.rightPart.leftPart.rightPart)).toBe(0);
+  describe("Preorder, inorder, postorder traversal methods", () => {
+    it("preorder - <root><left><right>", () => {
+      newTree.preorder(newTree.root);
+      expect(newTree.preorderData).toEqual([
+        45, 6, 2, 5, 23, 21, 35, 235, 52, 62, 345, 236, 346,
+      ]);
+    });
+
+    it("this method should traverse the tree like this <left><root><right>", () => {
+      newTree.inorder(newTree.root);
+      expect(newTree.inorderData).toEqual([
+        2, 5, 6, 21, 23, 35, 45, 52, 62, 235, 236, 345, 346,
+      ]);
+    });
+
+    it("this method should traverse the tree like this <left><right><root>", () => {
+      newTree.postorder(newTree.root);
+      expect(newTree.postorderData).toEqual([
+        5, 2, 21, 35, 23, 6, 62, 52, 236, 346, 345, 235, 45,
+      ]);
+    });
   });
 
-  it("height method should return how deep the given node is", () => {
-    expect(newTree.height(newTree.root)).toBe(3);
+  describe("depth method", () => {
+    it("depth of an empty tree should be equal to -1", () => {
+      expect(newTree.depth(null)).toBe(-1);
+    });
+
+    it("depth of root's tree should be equal to 0", () => {
+      expect(newTree.depth(newTree.root)).toBe(0);
+    });
+
+    it("depth of root's child should be equal to 1", () => {
+      expect(newTree.depth(newTree.root.leftPart)).toBe(1);
+    });
+
+    it("depth of root's child child should be equal to 1", () => {
+      expect(newTree.depth(newTree.root.leftPart.leftPart)).toBe(2);
+    });
+  });
+
+  describe("isBalanced method", () => {
+    it("newTree should be balanced", () => {
+      expect(newTree.isBalanced(newTree.root)).toBeTruthy();
+    });
+    //add more tests
+  });
+
+  describe("rebalance method", () => {
+    it("newTree should be balanced", () => {
+      expect(newTree.isBalanced(newTree.root)).toBeTruthy();
+    });
   });
 });
