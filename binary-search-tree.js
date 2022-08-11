d5b2e3e442c5c6c022c93450c79d32a5ce328744
lnicepei <dmitrii.shaidorov@gmail.com>
2022-08-11 12:06:19 +0300

    Implement many methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,5 +1,3 @@
-// import "./style.css";
-
 class Node {
   constructor(data) {
     this.data = data;
@@ -12,12 +10,15 @@ class Tree {
   constructor(array) {
     this.array = [...removeDuplicates(mergeSort(array))];
     this.root = this.buildTree(this.array, 0, this.array.length - 1);
+    this.preorderData = [];
+    this.inorderData = [];
+    this.postorderData = [];
   }
 
   buildTree(array, start, end) {
     if (start > end) return null;
-    let mid = parseInt((start + end) / 2);
 
+    let mid = parseInt((start + end) / 2);
     let root = new Node(array[mid]);
 
     root.leftPart = this.buildTree(array, start, mid - 1);
@@ -44,7 +45,7 @@ class Tree {
     if (root.leftPart !== null) {
       this.traverse(root.leftPart);
     }
-    console.log(root.data);
+
     if (root.rightPart !== null) {
       this.traverse(root.rightPart);
     }
@@ -82,28 +83,116 @@ class Tree {
       root.data = minValue(root);
       root.rightPart = this.delete(root.rightPart, root.data);
     }
+
     return root;
   }
 
-  levelOrder(accumulator) {
-    
-  } 
+  levelOrder(root) {
+    const queue = [];
+    const result = [];
+
+    if (root == null) return;
+
+    queue.push(root);
+
+    while (queue.length > 0) {
+      let current = queue.shift(root);
+      result.push(current.data);
+
+      if (current.leftPart !== null) queue.push(current.leftPart);
+      if (current.rightPart !== null) queue.push(current.rightPart);
+    }
+
+    return result;
+  }
+
+  preorder(root) {
+    if (root == null) return;
+
+    if (root.data !== undefined) {
+      this.preorderData.push(root.data);
+    }
+
+    if (root.leftPart !== null) {
+      this.preorder(root.leftPart);
+    }
+
+    if (root.rightPart !== null) {
+      this.preorder(root.rightPart);
+    }
+  }
+
+  inorder(root) {
+    if (root == null) return;
+
+    if (root.leftPart !== null) {
+      this.inorder(root.leftPart);
+    }
+
+    if (root.data !== undefined) {
+      this.inorderData.push(root.data);
+    }
+
+    if (root.rightPart !== null) {
+      this.inorder(root.rightPart);
+    }
+  }
+
+  postorder(root) {
+    if (root == null) return;
+
+    if (root.leftPart !== null) {
+      this.postorder(root.leftPart);
+    }
+
+    if (root.rightPart !== null) {
+      this.postorder(root.rightPart);
+    }
+
+    if (root.data !== undefined) this.postorderData.push(root.data);
+  }
 
   height(root) {
     if (root == null) {
-      return 0;
+      return -1;
     } else {
-
       let left = this.height(root.leftPart);
       let right = this.height(root.rightPart);
 
-      if (left > right) {
-        return right + 1;
-      } else {
-        return left + 1;
-      }
+      return Math.max(left, right) + 1;
     }
   }
+
+  depth(node, root = this.root) {
+    let depth = -1;
+
+    if (node == null) return depth;
+
+    if (
+      root == node ||
+      (depth = this.depth(node, root.leftPart)) >= 0 ||
+      (depth = this.depth(node, root.rightPart) >= 0)
+    ) {
+      return depth + 1;
+    }
+
+    return depth;
+  }
+
+  isBalanced(root) {
+    if (root == null) return false;
+
+    let leftHalf = root.leftPart;
+    let rightHalf = root.rightPart;
+
+    if (Math.abs(this.height(leftHalf) - this.height(rightHalf)) > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  rebalance(root) {}
 }
 
 function minValue(root) {
@@ -136,10 +225,6 @@ function mergeSort(array) {
 
 function removeDuplicates(array) {
   return [...new Set(array)];
-}
-
-function accumulator(element) {
-  alert(element);
 }
 
 export { Tree };
